f34288720b0888204c031af04c50aa2e06c656bc
Mashhad Ali Shah <165510733+MASHHADALI@users.noreply.github.com>
2024-09-06 05:47:33 +0500

    Add files via upload

---
+++ hell.js
@@ -0,0 +1,192 @@
+// TASK-1
+// let name = prompt("enter your name")
+// alert("welcome to the webpage" + " " +name)
+
+
+// TASK-2
+// let numberone = +prompt("enter the first number")
+// let numbertwo = +prompt("enter the second number")
+// alert(numberone + numbertwo)
+
+
+
+// TASK-3
+// LEGAL VARIABLES
+// Can have letters , Underscore , numbers after letters , Dollar sign
+
+// let name = ("mashhad")
+// let NAME1 = ("mashhad")
+// let _name = ("mashhad")
+// let $name = ("mashhad")
+
+// IILEGAL VARIABLES
+
+// cannot start with a number
+// let 1name = ("mashhad")
+
+// canot use hypens
+// let name- = ("mashhad")
+
+// can not use space in between
+// let my name = ("mashhad")
+
+
+
+
+
+
+// TASK-4 CALCULATOR
+// let operator = prompt("enter operator(either +,-,*,/)")
+// let num1 = parseFloat(prompt("enter first no"))
+// let num2 = parseFloat(prompt("enter second number"))
+
+// if (operator == "+") {alert(num1 + num2)}
+// else if (operator == "-"){alert( num1 - num2)}
+// else if (operator == "*"){alert(num1 * num2)}
+// else  { alert(num1/num2)} ;
+// console.log("${num1} ${operator} ${num2} = ${alert}")
+
+
+
+// TASK-5
+// let no1 = prompt("enter no1")
+// let no2 = prompt("enter no2")
+// let modulus = no1 % no2 
+// alert("the remainder of"  + " "+  no1+ " "+ "divided by" +" "+  no2 + " "+  "is" + " " + modulus)
+
+
+
+
+
+
+// TASK-6
+// let num1 = 9
+// let num2 = 12
+// num1++
+// num2--
+// alert (num1 )
+// alert (num2)
+
+
+
+
+
+// TASK-7
+// let calc =( 30+30 -20 /40*2)
+// alert(calc)
+
+
+
+
+
+// TASK-8
+// let first = ("HELLO")
+// let second = (" Mr")
+// let third = (" bla bla ")
+// alert (first + second + third)
+
+
+// TASK-9
+// let name = prompt("What is your name")
+// let age = prompt("Enter your age")
+// alert("Welcome" + " "+name + " " + "You are "+ " " + age + " " +"years old") 
+
+
+// TASK-10
+// let number = prompt("enter a number")
+// if(number == '0'){alert("zero")}
+// else if (number < 0){alert ("negative")}
+// else if (number > 0){alert("positive")}
+// else(alert("wrong"))
+
+
+
+// TASK-11
+// let firstno = prompt ("enter 1st number")
+// let secondno = prompt ("enter 2nd number")
+// if (firstno  > secondno){alert("1st no is greater than secondno")}
+// else if (firstno <secondno) {alert("1st no is less than 2nd no")}
+// else(alert("equal no"))
+
+
+// TASK-12
+// let score = prompt("enter your score")
+// if (score >=90){alert("A+")}
+// else if (score >=80){alert("A")}
+// else if (score >=70){alert("B+")}
+// else if (score >=60){alert("B")}
+// else if (score >=50){alert("POOR")}
+// else  (alert("FAIL"))
+
+
+
+
+
+
+
+// TASK-13
+// let age = prompt ("enter age")
+// let boy = prompt("are you student")
+// if (age >= 19 && boy == "yes") {alert("u can apply")
+// }  
+// else (alert("you cannot apply") )
+
+
+
+
+
+
+
+
+// TASK-14
+// let pass =prompt("enter pass")
+// let username =prompt("enter username")
+
+// if (pass=="123"){if(username=="user"){alert("welcome")}}
+
+// else if (pass !== "123"){if (username=="user"){alert("pass is incorrect")}}
+
+
+
+// TASK-15
+// let fruits = ["apple" , "banana", "orange","guava","mango" ]
+// for (let i = 0 ; i < 5 ; i++){alert(fruits[i])}
+
+
+
+
+
+
+// TASK-16
+// let fruits = ["apple" , "banana", "orange","guava","mango" ]
+
+// // ADD ELEMENTS
+// fruits.push("starwberry")
+// fruits.unshift("Pomegranate")
+
+// // REMOVE ELEMENTS
+// // fruits.pop()
+// fruits.shift()
+
+
+// alert (fruits)
+
+
+// TASK-17]
+// let array =["KHAN","ABDULLAH","WAQAR","AHMAD","NASEEM","ALI"]
+// let middleIndex = Math.floor(array.length / 2);
+// array=array.slice(1)
+// alert(array)
+
+// let num = ["1", "2", "3" ,"4" ,"5"]
+// num.splice(2,2, "8","9")
+// console.log(num)
+
+
+
+
+
+
+// TASK-18
+// for(i=0; i<=10;i++){
+//     console.log(i)}
